1ab086fc7441ffa1e6d37b35bd9de91775cd36b1
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-01-04 07:37:45 +0200

    Added routing to features modules

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -1,16 +1,17 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 import { AuthService } from './auth.service';
 
 @Component({
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
 })
 export class LoginComponent {
   errorMessage = '';
   pageTitle = 'Log In';
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) {}
 
   login(loginForm: NgForm): void {
     if (loginForm && loginForm.valid) {
@@ -18,7 +19,7 @@ export class LoginComponent {
       const password = loginForm.form.value.password;
       this.authService.login(userName, password);
 
-      // Navigate to the Product List page after log in.
+      this.router.navigateByUrl('/products');
     } else {
       this.errorMessage = 'Please enter a user name and password.';
     }
