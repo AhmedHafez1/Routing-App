5d835af08e8c4092e23e042c2d1b0f0017472b58
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-01-15 07:39:17 +0200

    Grouping with Componentless route

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -14,20 +14,25 @@ import { ProductResolver } from './product.resolver';
   imports: [
     SharedModule,
     RouterModule.forChild([
-      { path: 'products', component: ProductListComponent },
       {
-        path: 'products/:id',
-        component: ProductDetailComponent,
-        resolve: { product: ProductResolver },
-      },
-      {
-        path: 'products/:id/edit',
-        component: ProductEditComponent,
-        resolve: { resolvedData: ProductResolver },
+        path: 'products',
         children: [
-          { path: '', redirectTo: 'info', pathMatch: 'full' },
-          { path: 'info', component: ProductEditInfoComponent },
-          { path: 'tags', component: ProductEditTagsComponent },
+          { path: '', component: ProductListComponent },
+          {
+            path: ':id',
+            component: ProductDetailComponent,
+            resolve: { product: ProductResolver },
+          },
+          {
+            path: ':id/edit',
+            component: ProductEditComponent,
+            resolve: { resolvedData: ProductResolver },
+            children: [
+              { path: '', redirectTo: 'info', pathMatch: 'full' },
+              { path: 'info', component: ProductEditInfoComponent },
+              { path: 'tags', component: ProductEditTagsComponent },
+            ],
+          },
         ],
       },
     ]),
